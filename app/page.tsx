fca2214621b55cc7e392807c8ff6a90f5a79c717
Kwin Palanuwech <kwin.palanuwech@kellogg.northwestern.edu>
2025-04-21 09:15:43 +0000

    Added QandA

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,15 @@ import { motion } from 'motion/react'
 import { XIcon } from 'lucide-react'
 import { Spotlight } from '@/components/ui/spotlight'
 import { Magnetic } from '@/components/ui/magnetic'
+import { InfiniteSlider } from '@/components/motion-primitives/infinite-slider';
+import { ProgressiveBlur } from '@/components/motion-primitives/progressive-blur';
+import {
+  Accordion,
+  AccordionItem,
+  AccordionTrigger,
+  AccordionContent,
+} from '@/components/motion-primitives/accordion';
+import { ChevronRight } from 'lucide-react';
 import {
   MorphingDialog,
   MorphingDialogTrigger,
@@ -135,14 +144,106 @@ export default function Personal() {
         variants={VARIANTS_SECTION}
         transition={TRANSITION_SECTION}
       >
-        <div className="flex-1">
-          <p className="text-zinc-600 dark:text-zinc-400">
-            Focused on creating intuitive and performant web experiences.
-            Bridging the gap between design and development.
-          </p>
-        </div>
+      <div className="flex-1">
+        <p className="text-zinc-600 dark:text-zinc-400">
+          We can't wait to share this special day with you!
+        </p>
+        <p className="text-zinc-600 dark:text-zinc-400">
+        December 9th, 2025
+        </p>
+        <p className="text-zinc-600 dark:text-zinc-400">
+          Capella Bangkok, Thailand
+        </p>
+      </div>
       </motion.section>
 
+      <motion.section
+        variants={VARIANTS_SECTION}
+        transition={TRANSITION_SECTION}
+      >
+      <h3 className="mb-5 text-lg font-medium">Q&A</h3>
+      <Accordion
+      className='flex w-full flex-col'
+      transition={{ type: 'spring', stiffness: 120, damping: 20 }}
+      variants={{
+        expanded: {
+          opacity: 1,
+          scale: 1,
+        },
+        collapsed: {
+          opacity: 0,
+          scale: 0.7,
+        },
+      }}
+    >
+      <AccordionItem value='getting-started' className='py-2'>
+        <AccordionTrigger className='w-full py-0.5 text-left text-zinc-950 dark:text-zinc-50'>
+          <div className='flex items-center'>
+            <ChevronRight className='h-4 w-4 text-zinc-950 transition-transform duration-200 group-data-expanded:rotate-90 dark:text-zinc-50' />
+            <div className='ml-2 text-zinc-950 dark:text-zinc-50'>
+              When is the RSVP deadline?
+            </div>
+          </div>
+        </AccordionTrigger>
+        <AccordionContent className='origin-left'>
+          <p className='pl-6 pr-2 text-zinc-500 dark:text-zinc-400'>
+            Please RSVP by October 9th, so we can have an accurate headcount. :)
+          </p>
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value='animation-properties' className='py-2'>
+        <AccordionTrigger className='w-full py-0.5 text-left text-zinc-950 dark:text-zinc-50'>
+          <div className='flex items-center'>
+            <ChevronRight className='h-4 w-4 text-zinc-950 transition-transform duration-200 group-data-expanded:rotate-90 dark:text-zinc-50' />
+            <div className='ml-2 text-zinc-950 dark:text-zinc-50'>
+              Can I bring a date?
+            </div>
+          </div>
+        </AccordionTrigger>
+        <AccordionContent className='origin-left'>
+          <p className='pl-6 pr-2 text-zinc-500 dark:text-zinc-400'>
+          Since our event is a sit-down lunch, seats will be fixed, so please check your invite for your +1!
+          </p>
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value='advanced-features' className='py-2'>
+        <AccordionTrigger className='w-full py-0.5 text-left text-zinc-950 dark:text-zinc-50'>
+          <div className='flex items-center'>
+            <ChevronRight className='h-4 w-4 text-zinc-950 transition-transform duration-200 group-data-expanded:rotate-90 dark:text-zinc-50' />
+            <div className='ml-2 text-zinc-950 dark:text-zinc-50'>
+              Are kids welcome?
+            </div>
+          </div>
+        </AccordionTrigger>
+        <AccordionContent className='origin-left'>
+          <p className='pl-6 pr-2 text-zinc-500 dark:text-zinc-400'>
+          As much as we love your little ones, we will not be including them in the ceremony or reception. 
+          However, we recognize that some of you will be traveling with your kids, so please know they are welcome 
+          at the Friday cocktail reception and Sunday brunch! We will also be providing a babysitting service at the 
+          venue during the ceremony if you cannot find other accommodations for them. Please reach out if you have questions!
+          </p>
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value='community-support' className='py-2'>
+        <AccordionTrigger className='w-full py-0.5 text-left text-zinc-950 dark:text-zinc-50'>
+          <div className='flex items-center'>
+            <ChevronRight className='h-4 w-4 text-zinc-950 transition-transform duration-200 group-data-expanded:rotate-90 dark:text-zinc-50' />
+            <div className='ml-2 text-zinc-950 dark:text-zinc-50'>
+            What will the weather be like?
+            </div>
+          </div>
+        </AccordionTrigger>
+        <AccordionContent className='origin-left'>
+          <p className='pl-6 pr-2 text-zinc-500 dark:text-zinc-400'>
+          Thailand in December is warm, breezy, and mostly rain-free. Think sunshine, 
+          clear skies, and perfect weather for a celebration. Be sure to pack accordingly and check 
+          the weather before your journey here! :)
+          </p>
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+      </motion.section>
+      
       <motion.section
         variants={VARIANTS_SECTION}
         transition={TRANSITION_SECTION}
